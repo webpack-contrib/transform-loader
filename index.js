ab0faad0d36990db0b4207bf8042e0ae977de47f
Tobias Koppers <tobias.koppers@googlemail.com>
2013-11-05 18:12:01 +0100

    streams are async, ever

--- index.js
+++ index.js
@@ -4,45 +4,33 @@
 */
 module.exports = function(input) {
 	if(!this.query) throw new Error("Pass a module name as query to the transform-loader.");
-	var callback = this.callback;
+	var callback = this.async();
 	var resource = this.resource;
 	var loaderContext = this;
 	var q = this.query.substr(1);
 	if(/^[0-9]+$/.test(q)) {
 		next(this.options.transforms[+q]);
 	} else {
-		if(this.async()) {
-			this.resolve(this.context, q, function(err, module) {
-				if(err) return callback(err);
-				next(require(module));
-			});
-		} else {
-			var module = this.resolveSync(this.context, q);
+		this.resolve(this.context, q, function(err, module) {
+			if(err) return callback(err);
 			next(require(module));
-		}
+		});
 	}
 	function next(transformFn) {
 		var stream = transformFn(resource);
 		var bufs = [];
-		var done = false;
 		stream.on("data", function(b) {
 			bufs.push(b);
 		});
 		stream.on("end", function() {
 			var b = Buffer.concat(bufs);
-			done = true;
-			console.log(b);
 			callback(null, b);
 		});
-		console.log("APPLY TRANSFORM TO " + input);
+		stream.on("error", function(err) {
+			callback(err);
+		});
 		stream.write(input);
 		stream.end();
-		if(!done) {
-			if(!loaderContext.async()) {
-				stream.removeListener("end");
-				throw new Error("The module system only accept sync mode, but the stream is not sync");
-			}
-		}
 	}
 }
 module.exports.raw = true;
