56c8a21492e8d4ece11928ff66a28c6c1fa9dcf8
shellscape <andrew@shellscape.org>
2018-04-06 17:55:14 -0400

    ci(jest): fix loader test

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,26 +1,22 @@
 /* eslint-disable
   prefer-destructuring,
 */
-// import loader from '../src/cjs';
-// import webpack from './helpers/compiler';
+import webpack from './helpers/compiler';
 
 describe('Loader', () => {
-  // this test throwing errors, and the errors have user-specific paths in their
-  // messages, which is causing issues with the snapshots across different
-  // environments
+  test('Defaults', async () => {
+    const config = {
+      module: {
+        rules: [{
+          test: /\.coffee?$/,
+          loader: `${__dirname}/../src/cjs?coffeeify`,
+        }],
+      },
+    };
 
-  // test('Defaults', async () => {
-  //   const config = {
-  //     loader: {
-  //       test: /\.coffee$/,
-  //       loader: `${loader}?coffeeify`,
-  //       options: {},
-  //     },
-  //   };
-  //
-  //   const stats = await webpack('fixture.js', config);
-  //   const { source } = stats.toJson().modules[1];
-  //
-  //   expect(source).toMatchSnapshot();
-  // });
+    const stats = await webpack('fixture.js', config);
+    const { source } = stats.toJson().modules[1];
+
+    expect(source).toMatchSnapshot();
+  });
 });
